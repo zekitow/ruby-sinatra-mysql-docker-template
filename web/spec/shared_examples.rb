9dc438c92acd11d32b0b253a85139de170dfbeef
José Ribeiro Filho <zeca@digup.io>
2018-09-20 20:00:48 -0300

    Update shared_examples.rb

--- web/spec/shared_examples.rb
+++ web/spec/shared_examples.rb
@@ -1,8 +1,3 @@
-
-#
-# Exemplo usado para verificar se model e valido, quando é removido o attributo "parameter"
-# Exemplo: ./spec/models/player_spec.rb
-#
 shared_examples "model without required attribute" do |model, parameter|
 
   subject { model.new(model_attributes.except(parameter.to_sym)).valid? }
@@ -10,10 +5,6 @@ shared_examples "model without required attribute" do |model, parameter|
 
 end
 
-#
-# Exemplo usado para verificar que um campo nao é buscavel.
-# Exemplo: ./spec/models/player_spec.rb
-#
 shared_examples "unsearchable attribute" do | search_term |
   subject { Player.search(search_term).results.size }
   it      { should eq(0) }
